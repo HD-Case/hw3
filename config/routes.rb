fdf888e4be000cec6e918abb05dc43df5acec2f6
Benard Haugen <ben.haugen@kellogg.northwestern.edu>
2025-02-26 09:17:33 +0000

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'entries/new'
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get("/", { :controller => "places", :action => "index" })
+
+  resources "places"
+  #rails generate controller Places index show new create
+  resources "entries"
+  #rails generate controller Entries new create
+  # don't need 'index' because we handle in places/show
 end
